bd76ab4fdfed8f641ab2b968dccb2b8621ec39ac
NicoTecno <nicodominguez1468@gmail.com>
2026-04-04 12:22:30 -0300

    Docker

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,4 +1,15 @@
 import postgres from 'postgres';
 import 'dotenv/config';
 
-export const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+//export const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
+
+export const sql = postgres(process.env.DATABASE_URL!);
+
+ // Al final del archivo, temporalmente:
+  sql`SELECT NOW()`.then(result => {
+    console.log('✓ DB conectada:', result[0].now);
+    // process.exit(0);
+  }).catch(err => {
+    console.error('✗ Falló la conexión:', err.message);
+    process.exit(1);
+  });
